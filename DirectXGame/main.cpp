109060bd83a8d673f3add4f08408e1cab31f9313
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2025-04-16 12:01:24 +0900

    00_00前準備完了

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,24 @@
 #include <Windows.h>
+#include "KamataEngine.h"
+
+
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 	
+	//初期化処理
+	// // エンジンの初期化
+
+	KamataEngine::Initialize(L"LE3C_26_ムラタ_トモキ");
+
+	//メインループ
+	while (true) {
+		if (KamataEngine::Update()) {
+			break;
+		}
+	}
+
+	// 終了処理
+	KamataEngine::Finalize();
 	return 0;
 }
