c5341e32a660c1c890a82b22c3825199502011b7
Tomoki0827223 <146914498+Tomoki0827223@users.noreply.github.com>
2025-04-16 12:06:08 +0900

    DirectXCommonのインスタンス管理を追加
    `using namespace KamataEngine;` を削除し、`DirectXCommon` のインスタンスを `dxCommon` ポインタで管理するように変更しました。メインループ内での描画処理を `dxCommon->PreDraw();` と `dxCommon->PostDraw();` で行うようにし、エンジンの初期化後に描画処理が適切に実行されるように改善しました。

--- DirectXGame/main.cpp
+++ DirectXGame/main.cpp
@@ -1,7 +1,7 @@
 #include <Windows.h>
 #include "KamataEngine.h"
 
-
+using namespace KamataEngine;
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
@@ -11,12 +11,22 @@ int WINAPI WinMain(_In_ HINSTANCE, _In_opt_ HINSTANCE, _In_ LPSTR, _In_ int) {
 
 	KamataEngine::Initialize(L"LE3C_26_ムラタ_トモキ");
 
+	DirectXCommon* dxCommon = DirectXCommon::GetInstance();
+
 	//メインループ
 	while (true) {
 		if (KamataEngine::Update()) {
 			break;
 		}
 	}
+
+	// 描画処理
+	dxCommon->PreDraw();
+
+
+
+	// 描画処理
+	dxCommon->PostDraw();
 
 	// 終了処理
 	KamataEngine::Finalize();
